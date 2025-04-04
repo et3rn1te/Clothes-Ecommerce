43e8d4cba21c56a787c264b3a999319f7c9f1a40
21130521 <ndt1011ai@gmail.com>
2025-04-04 11:35:39 +0700

    second commit
    Thêm các dependencies sau:
    Spring Web
    Spring Data JPA
    MySQL Driver
    Lombok (để giảm boilerplate code)
    Spring Security

---
+++ back_end/src/test/java/com/example/clothes_ecommerce/ClothesEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clothes_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
