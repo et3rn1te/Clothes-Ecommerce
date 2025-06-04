90c6dc81ba8f0780fddea1ce2a27c99e4b185b71
LeThanhTam7723 <tamle7723@gmail.com>
2025-06-04 16:02:51 +0700

    order
    xóa cart sau thanh thanh toán

--- back_end/src/main/java/com/example/back_end/repository/CartRepository.java
+++ back_end/src/main/java/com/example/back_end/repository/CartRepository.java
@@ -2,7 +2,11 @@ package com.example.back_end.repository;
 
 import com.example.back_end.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,5 +14,4 @@ import java.util.Optional;
 public interface CartRepository extends JpaRepository<Cart,Long> {
     Optional<Cart> findByUser_IdAndIsOrdered(Long idUser,boolean ordered);
     Optional<Cart> findByUser_Id(Long idUser);
-
 }
